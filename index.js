7670745a5fbaf28d419e63c2dc988d2605722a39
mithra-1998 <mitworkmail@gmail.com>
2020-03-05 08:59:13 +0530

    1 file modified

--- index.js
+++ index.js
@@ -132,6 +132,23 @@ console.log(checkVowel('o',arr)) //true
 
 //check if a number is prime 
 
+checkPrime = (n) => {
+  if(n === 1)
+  return false;
+  else if(n === 2)
+  return true;
+  else {
+    for(let i = 2; i <= Math.sqrt(n); i++)
+    if(n%i === 0)
+    return false;
+  }
+  return true;
+}
+
+console.log(checkPrime(81)) //Output : false
+console.log(checkPrime(3)) //Output : true
+  
+
 
 
 
